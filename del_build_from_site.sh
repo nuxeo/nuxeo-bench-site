b9d352173eefc16825434412990b71447b58cd2e
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-01-18 16:35:36 +0100

    fix delete build from site

--- del_build_from_site.sh
+++ del_build_from_site.sh
@@ -4,19 +4,23 @@ set -x
 cd $(dirname $0)
 # fail on any command error
 set -e
-BUILD_PATH=$1
-shift
 SITE_PATH=$1
 shift
-DATA_FILE=$BUILD_PATH/archive/reports/data.yml
+BUILD_ID=$1
+shift
 
-function get_artifact_info() {
+function search_build() {
+  DATA_FILE=`grep -l "build_number: ${BUILD_ID}$" $SITE_PATH/data/bench/*.yml`
+  if [ -z "$DATA_FILE" ]; then
+    echo "No data file match build_number: $BUILD_ID"
+    exit -1
+  fi
   # need dbprofile, benchid, benchname
-  export BENCHID=`grep benchid $DATA_FILE | cut -d \: -f 2 | sed 's,[" ],,g'`
   export DBPROFILE=`grep dbprofile $DATA_FILE | cut -d \: -f 2 | sed 's,",,g;s,^ *,,g'`
   export BENCHNAME=`grep benchname $DATA_FILE | cut -d \: -f 2 | sed 's,",,g;s,^ *,,g'`
-  export BUILDID=`grep build_number $DATA_FILE | cut -d \: -f 2 | sed 's,",,g;s,^ *,,g'`
+  export BENCHID=`grep benchid $DATA_FILE | cut -d \: -f 2 | sed 's,[" ],,g'`
   export BENCHFILE="$DBPROFILE$BUILDID"
+  echo "## Found build_number $BUILD_ID: $DBPROFILE - $BENCHID - $BENCHNAME"
 }
 
 function del_build() {
@@ -29,6 +33,6 @@ function del_build() {
 # -------------------------------------------------------
 # main
 #
-get_artifact_info
+search_build
 del_build
 echo "Done"

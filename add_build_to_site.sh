b37f4cd9db46879d26885010f8f364e9fd3b07a2
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-03-18 22:51:59 +0100

    NXBT-1022 make classifier modifiable per report, analyze audit/ft results

--- add_build_to_site.sh
+++ add_build_to_site.sh
@@ -23,6 +23,7 @@ function get_artifact_info() {
   export BENCH_DATE=`grep import_date $DATA_FILE | cut -d \: -f 2- | sed 's,",,g;s,^ *,,g;s,\ ,T,g'`
   export DBPROFILE=`grep dbprofile $DATA_FILE | cut -d \: -f 2 | sed 's,",,g;s,^ *,,g'`
   export NUXEONODES=`grep nuxeonodes $DATA_FILE | cut -d \: -f 2 | sed 's,",,g;s,^ *,,g'`
+  export CLASSIFIER=`grep classifier $DATA_FILE | cut -d \: -f 2 | sed 's,",,g;s,^ *,,g'`
   export DEFAULT_CATEGORY=`grep default_category $DATA_FILE | cut -d \: -f 2 | sed 's,",,g;s,^ *,,g'`
   if [ -z $CATEGORY ]; then
     if [ -z $DEFAULT_CATEGORY ]; then
@@ -73,8 +74,9 @@ title: "$DBPROFILE $NUXEONODES $BUILD_NUMBER"
 bench_suite: "$BENCH_SUITE"
 build_number: "$BUILD_NUMBER"
 dbprofile: "$DBPROFILE"
+classifier: "$CLASSIFIER"
 date: $BENCH_DATE
-type: bench
+type: "bench"
 ---
 EOF
 }

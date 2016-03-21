97f9c70616028785c5af5d44e37ee6ade3cccc95
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-03-21 14:43:49 +0100

    NXBT-1022 git pull bis

--- del_build_from_site.sh
+++ del_build_from_site.sh
@@ -14,6 +14,7 @@ function help {
 
 function del_build() {
   git checkout master
+  git pull
   git rm ./data/bench/$BUILD_NUMBER.yml ./content/*/$BUILD_NUMBER.md || true
   git commit -m"Removing build $BUILD_NUMBER"  || true
   rm -f ./data/bench/$BUILD_NUMBER.yml ./content/*/$BUILD_NUMBER.md || true

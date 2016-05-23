63f0257e550dab1721c2c6eed42b3b6592150d1b
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-05-23 16:06:19 +0200

    Upload the public site to s3

--- update_site.sh
+++ update_site.sh
@@ -4,6 +4,14 @@ cd $(dirname $0)
 # fail on any command error
 set -e
 
+
+function help {
+  echo "Build the benchmark site with hugo, deploy on S3"
+  echo "Usage: $0 [S3_BUCKET]"
+  exit 0
+}
+
+
 hugo_path=$(which hugo) || true
 if [ -x "$hugo_path" ] ; then
   HUGO=hugo
@@ -15,7 +23,30 @@ function rebuild_site() {
   $HUGO --theme=hyde
 }
 
+function sync_s3() {
+  local S3_BUCKET=$1
+  shift
+  time aws s3 sync ./public/ $S3_BUCKET/
+}
+
 # -------------------------------------------------------
 # main
 #
+while getopts "h" opt; do
+    case $opt in
+        h)
+            help
+            ;;
+        ?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+shift $(($OPTIND - 1))
+S3_BUCKET=$1
+
 rebuild_site
+if [ ! -z $S3_BUCKET ]; then
+  sync_s3 $S3_BUCKET
+fi

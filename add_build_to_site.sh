acb9afc9b67a2126cb44aa9a5799aba82e85deb4
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-09-02 19:00:37 +0200

    Remove public s3 access

--- add_build_to_site.sh
+++ add_build_to_site.sh
@@ -74,7 +74,8 @@ function copy_build() {
 function copy_build_s3() {
    gzip $BUILD_SRC_PATH/log || true
    #time s3cmd sync $BUILD_SRC_PATH $SITE_PATH/build/
-   time aws s3 sync $BUILD_SRC_PATH $BUILD_DEST_PATH/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+   #time aws s3 sync $BUILD_SRC_PATH $BUILD_DEST_PATH/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+   time aws s3 sync $BUILD_SRC_PATH $BUILD_DEST_PATH/
    gunzip $BUILD_SRC_PATH/log.gz || true
 }
 
